1bb776ccfa22731bd70c1878f7e4a18a4449df54
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-09-29 22:58:04 +0530

    Update calculate_Square.py

--- calculate_Square.py
+++ calculate_Square.py
@@ -1,2 +1,2 @@
 n = input("Pick a Number, Any number: ")
-print "Did You know that " + str(n) + " squared is " + str(n*n) + "?"
+print "Did You Know that " + str(n) + " squared is " + str(n*n) + "?"
