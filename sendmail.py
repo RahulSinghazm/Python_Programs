d97623451f86224c171b388d418e2d3da23de2e2
RahulSinghazm <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-09 01:41:15 +0530

    commit

---
+++ sendmail.py
@@ -0,0 +1,28 @@
+#First way Program of Sending mail using python
+
+
+
+#import smtplib
+#content="Hi, Bro how are you?"
+#mail=smtplib.SMTP('smtp,gmail.com',587)
+#mail.ehlo()
+#mail.starttls()
+#mail.login('singh@gmail.com','050793@Rs')
+#mail.sendmail('singh1@gmail.com','singh2@gmail.com',content)
+#mail.close()
+
+
+
+#Second way Program of Sending mail using python
+
+
+
+import smtplib
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login('singh1@gmail.com','Password')
+ 
+msg = "YOUR MESSAGE!"
+server.sendmail('singh1@gmail.com','singh2@gmail.com',msg)
+server.quit()
