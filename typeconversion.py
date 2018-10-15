3dd8956d36252b19a0c15fec11620b0cd387cc0c
RahulSinghazm <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-15 13:18:00 +0530

    commit

--- typeconversion.py
+++ typeconversion.py
@@ -1,3 +1,4 @@
+#in input function taking all DATA TYPE
 a=input('Enter number:')
 print('Enter input type is:',a)
 
@@ -8,17 +9,10 @@ print('Enter input type is:',type(r))
 
 
 s=float(a)
-print('Enter input:',r)
+print('Enter input:',s)
 print('Enter input type is:',type(s))
 
 
 t=complex(a)
-print('Enter input:',r)
+print('Enter input:',t)
 print('Enter input type is:',type(t))
-
-
-u=str(a)
-print('Enter input:',r)
-print('Enter input type is:',type(u))
-
-
