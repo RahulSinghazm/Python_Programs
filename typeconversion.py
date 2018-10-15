dac440ddeb6a0372d745200c0721c22e7099215e
RahulSinghazm <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-15 13:08:38 +0530

    commit

---
+++ typeconversion.py
@@ -0,0 +1,24 @@
+a=input('Enter number:')
+print('Enter input type is:',a)
+
+
+r=int(a)
+print('Enter input:',r)
+print('Enter input type is:',type(r))
+
+
+s=float(a)
+print('Enter input:',r)
+print('Enter input type is:',type(s))
+
+
+t=complex(a)
+print('Enter input:',r)
+print('Enter input type is:',type(t))
+
+
+u=str(a)
+print('Enter input:',r)
+print('Enter input type is:',type(u))
+
+
